55a14e2f5fda7a65adeced62f9d5886e52eeeff9
amankarki151 <amankarki151@gmail.com>
2024-08-06 20:48:51 +0530

    Update multiplediseaseprediction.py

--- Multiple-Disease-Prediction-System-aman/multiplediseaseprediction.py
+++ Multiple-Disease-Prediction-System-aman/multiplediseaseprediction.py
@@ -7,9 +7,9 @@ from streamlit_option_menu import option_menu
 
 diabetes_model = pickle.load(open('Multiple-Disease-Prediction-System-aman/diabetes_model.sav', 'rb'))
 
-heart_disease_model = pickle.load(open('heart_disease_model.sav','rb'))
+heart_disease_model = pickle.load(open('Multiple-Disease-Prediction-System-aman/heart_disease_model.sav','rb'))
 
-parkinsons_model = pickle.load(open('parkinsons_model.sav', 'rb'))
+parkinsons_model = pickle.load(open('', 'rb'))
 
 
 
